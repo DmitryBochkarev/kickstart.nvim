ed7df11e8b0f67a6a275eb9ff19bc9ed6d8c60ad
Dmitry Bochkarev <dimabochkarev@gmail.com>
2024-03-04 18:50:00 +0100

    enable some lsp

--- init.lua
+++ init.lua
@@ -621,18 +621,18 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
+  rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
-  --   lua_ls = {
-  --     Lua = {
-  --       workspace = { checkThirdParty = false },
-  --       telemetry = { enable = false },
-  --       -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-  --       -- diagnostics = { disable = { 'missing-fields' } },
-  --     },
-  --   },
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+      -- diagnostics = { disable = { 'missing-fields' } },
+    },
+  },
 }
 
 -- Setup neovim lua configuration
