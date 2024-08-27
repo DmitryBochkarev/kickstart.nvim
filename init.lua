0ee8d3f0d2b55235c5a3132b0742ac2f755a2dba
Dmitry Bochkarev <dimabochkarev@gmail.com>
2024-08-27 15:08:50 +0200

    multicursor

--- init.lua
+++ init.lua
@@ -297,6 +297,24 @@ require('lazy').setup({
   },
 
   {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      'smoka7/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+      {
+        mode = { 'v', 'n' },
+        '<leader>m',
+        '<cmd>MCstart<cr>',
+        desc = 'Create a selection for selected text or word under the cursor',
+      },
+    },
+  },
+
+  {
     'vladdoster/remember.nvim',
     config = function()
       require('remember')
